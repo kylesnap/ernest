f22a91fd34480a775e29429d2f1ddffb5f7ca3c1
Kyle Dewsnap <30324420+kylesnap@users.noreply.github.com>
2026-05-20 12:17:46 -0700

    Add parallel_prior function.

--- R/parallelization.R
+++ R/parallelization.R
@@ -22,6 +22,42 @@ parallel_likelihood <- function(
   )
 }
 
+parallel_prior <- function(
+  point_fn,
+  vectorized_fn,
+  ...,
+  .names,
+  .lower = -Inf,
+  .upper = Inf,
+  .repair = c(
+    "unique",
+    "universal",
+    "check_unique",
+    "unique_quiet",
+    "universal_quiet"
+  )
+) {
+  check_parallel_support()
+  interface <- check_exclusive(point_fn, vectorized_fn)
+  fn <- switch(
+    interface,
+    "point_fn" = point_fn,
+    "vectorized_fn" = vectorized_fn
+  )
+  fn <- carrier::crate(
+    set_env(fn),
+    !!!list2(...)
+  )
+  new_ernest_prior(
+    fn,
+    names = .names,
+    lower = .lower,
+    upper = .upper,
+    interface = interface,
+    .repair = .repair
+  )
+}
+
 check_parallel_support <- function() {
   check_installed(
     c("mirai", "carrier"),
--- tests/testthat/test-parallelization.R
+++ tests/testthat/test-parallelization.R
@@ -1,26 +1,64 @@
 test_that("parallel ernest_likelihood works", {
   test_matrix <- matrix(c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5), nrow = 2)
 
-  ll <- parallel_likelihood(function(x) {
-    sum(x)
-  })
-  it("produces scalar likelihoods", {
-    expect_s3_class(ll, c("ernest_likelihood", "crate"))
-    expect_equal(attr(ll, "interface"), "scalar_fn")
-    expect_equal(ll(c(0.0, 0.2, 0.4)), 0.6)
-    expect_equal(ll(test_matrix), c(0.6, 0.9))
-  })
+  ll <- parallel_likelihood(
+    function(x) {
+      sum(x) + c
+    },
+    c = 0.5
+  )
+  expect_s3_class(ll, c("ernest_likelihood", "crate"))
+  expect_equal(attr(ll, "interface"), "scalar_fn")
+  expect_equal(ll(c(0.0, 0.2, 0.4)), 1.1)
+  expect_equal(ll(test_matrix), c(1.1, 1.4))
 
   mat_ll <- parallel_likelihood(
     vectorized_fn = function(x) {
-      rowSums(x) + normalization
+      rowSums(x) + c
     },
-    normalization = 0.5
+    c = 0.5
   )
-  it("produces likelihood from `vectorized_fn`", {
-    expect_s3_class(mat_ll, c("ernest_likelihood", "crate"))
-    expect_equal(attr(mat_ll, "interface"), "vectorized_fn")
-    expect_equal(mat_ll(c(0.0, 0.2, 0.4)), 1.1)
-    expect_equal(mat_ll(test_matrix), c(1.1, 1.4))
-  })
+  expect_s3_class(mat_ll, c("ernest_likelihood", "crate"))
+  expect_equal(attr(mat_ll, "interface"), "vectorized_fn")
+  expect_equal(mat_ll(c(0.0, 0.2, 0.4)), 1.1)
+  expect_equal(mat_ll(test_matrix), c(1.1, 1.4))
+})
+
+test_that("parallel ernest_prior works", {
+  test_matrix <- matrix(c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5), nrow = 2)
+
+  pr <- parallel_prior(
+    function(x) {
+      cumsum(x) + c
+    },
+    .names = c("a", "b", "c"),
+    c = 0.5
+  )
+  expect_s3_class(attr(pr, "body"), "crate")
+  expect_equal(attr(pr, "interface"), "point_fn")
+  expect_equal(
+    pr$fn(c(0.0, 0.2, 0.4)),
+    matrix(c(0.5, 0.7, 1.1), nrow = 1)
+  )
+  expect_equal(
+    pr$fn(test_matrix),
+    matrix(c(0.5, 0.6, 0.7, 0.9, 1.1, 1.4), nrow = 2)
+  )
+
+  mat_pr <- parallel_prior(
+    vectorized_fn = function(x) {
+      matrixStats::rowCumsums(x) + 0.5
+    },
+    .names = c("A", "B", "C")
+  )
+  expect_s3_class(attr(mat_pr, "body"), "crate")
+  expect_equal(attr(mat_pr, "interface"), "vectorized_fn")
+  expect_equal(
+    pr$fn(c(0.0, 0.2, 0.4)),
+    matrix(c(0.5, 0.7, 1.1), nrow = 1)
+  )
+  expect_equal(
+    pr$fn(test_matrix),
+    matrix(c(0.5, 0.6, 0.7, 0.9, 1.1, 1.4), nrow = 2)
+  )
 })
